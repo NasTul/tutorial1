4421345a012d495a65b8e6e1ada8124729f70b00
Nastul <1113877138@qq.com>
2024-10-28 10:13:59 +1000

    Initial commit

---
+++ CircleArea.java
@@ -0,0 +1,34 @@
+
+/**
+ * Write a description of class CircleArea here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+
+import java.util.Scanner;
+
+public class CircleArea
+{   
+    
+    private double radius;
+    
+
+ 
+    public static void main(String[] args){
+        
+        
+        System.out.println("Hello world");
+        Scanner getinput = new Scanner(System.in);
+        
+        double radius = getinput.nextDouble();
+        double pi = 3.14;
+        double area = radius * radius * pi;
+        System.out.println(area);
+    
+    }
+    
+
+    
+    
+}
